8b38676c6019d7e001cac3cbe21423a2d71c5051
dawidkal <48098684+dawidkal@users.noreply.github.com>
2023-06-06 20:48:52 +0200

    Add files via upload

---
+++ JdbcConfig.java
@@ -0,0 +1,23 @@
+package pl.mojetesty;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("pl.mojetesty")
+public class JdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test_schema?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("dawid");
+
+        return dataSource;
+    }
+}
